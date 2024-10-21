b663ca233809ec49daa84dc1de83469fbc29a7c3
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2024-10-21 13:16:51 +0600

    Data inserted successfully into database

---
+++ AdoNetExample/AdonetUtility.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetExample
+{
+    public class AdonetUtility
+    {
+        private readonly string _connectionString = "Server=DESKTOP-8DI1SVB\\SQLEXPRESS;Database=CSharpB18;User Id=csharpb18;Password=123456; Trust Server Certificate=True ";
+        public void RunSql(string sql)
+        {
+            SqlConnection sqlConnection= new SqlConnection(_connectionString);
+            SqlCommand sqlCommand= new SqlCommand(sql,sqlConnection);
+
+            if(sqlConnection.State != System.Data.ConnectionState.Open)
+                sqlConnection.Open();
+
+
+            int rowExpected = sqlCommand.ExecuteNonQuery();
+
+            sqlConnection.Close();
+            sqlConnection.Dispose();
+            sqlCommand.Dispose();
+
+        }
+    }
+}
--- AdoNetExample/Program.cs
+++ AdoNetExample/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdoNetExample;
+
+AdonetUtility adonetUtility = new AdonetUtility();
+var sql = "insert into Student (name,dateofBirth,ismarried) values ('Jaladuddin','2/2/1998',1)";
+adonetUtility.RunSql(sql);
