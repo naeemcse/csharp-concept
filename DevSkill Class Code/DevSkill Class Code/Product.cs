ba2479995aaf2270158f3b89fa7d2bbc4c7a4fcd
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2024-12-18 15:24:20 +0600

    class 2 is completed with reflection

---
+++ DevSkill Class Code/DevSkill Class Code/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill_Class_Code
+{
+    public class Product : IPurchasable
+    {
+        public string Name {get;set;}
+        public double Price {get;set;}
+        public double Tax {get;set;}
+
+        public Product(string name , double price , double tax) 
+        { 
+            Name = name;
+            Price = price;
+            Tax = tax;
+        }
+
+      
+
+        public double CalculatePriceAfterTax()
+        {
+            return Price+Tax ;
+        }
+
+        public double CalculateDiscount(double discount)
+        {
+            return Price*discount/100;
+        }
+    }
+}
---
+++ DevSkill Class Code/DevSkill Class Code/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+
+Assembly a = Assembly.GetExecutingAssembly();
+var types = a.GetTypes();
+
+foreach(Type t in types)
+{
+   var interfaces = t.GetInterfaces();
+    if (interfaces.Any(x => x.Name == "IPurchasable"))
+    {
+
+        ConstructorInfo? constructor = t?.GetConstructor([typeof(string), typeof(double), typeof(double)]);
+        object? o = constructor?.Invoke(["Camera",3000,10]);
+
+        MethodInfo? method1 = t?.GetMethod("CalculatePriceAfterTax",BindingFlags.Instance | BindingFlags.Public);
+        MethodInfo? method2 = t?.GetMethod("CalculateDiscount", BindingFlags.Instance | BindingFlags.Public);
+
+
+        double? r1 = (double?)method1.Invoke(o, []) ;
+        double? r2 = (double?)method2.Invoke(o, [5]) ;
+
+        Console.WriteLine(r1);
+        Console.WriteLine(r2);
+
+    }
+
+
+
+
+}
+
+ 
