1caad6b59f98d298e8199be480aae86693a29452
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2024-12-21 13:58:22 +0600

    Lisk List is added

---
+++ FileAndDirectory/Program.cs
@@ -0,0 +1,26 @@
+﻿// file : video of class 18
+
+
+
+// Directory
+// Creat
+// Directory.CreateDirectory("Videos/Training");
+
+// Current path
+string crntPath = Directory.GetCurrentDirectory();
+Console.WriteLine(crntPath);
+
+// concatinatio of path
+
+Directory.SetCurrentDirectory(Path.Combine(crntPath,"Videos/Training"));
+File.WriteAllText("sample.txt", "HEllow Bangladesh");
+
+
+
+
+
+
+
+
+
+
---
+++ LinkList/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace LinkList
+{ 
+ class Program
+{
+
+
+    class LinkedListNode
+    {
+        public int data;
+        public LinkedListNode next;
+        public LinkedListNode(int x)
+        {
+            data = x;
+            next = null;
+        }
+
+    }
+
+    class LinkedList
+    {
+        int count = 0;
+        public LinkedListNode? head;
+        public LinkedList()
+        {
+            head = null;
+            count = 0;
+        }
+
+        public void AddNodeToFront(int data)
+        {
+
+            LinkedListNode? node = new LinkedListNode(data);
+            node.next = head;
+            head = node;
+            count++;
+
+        }
+
+        public void PrintList()
+        {
+            LinkedListNode runner = head;
+            while (runner != null)
+            {
+                Console.WriteLine(runner.data);
+                runner = runner.next;
+            }
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        LinkedList list = new LinkedList();
+        list.AddNodeToFront(1);
+        list.AddNodeToFront(2);
+        list.AddNodeToFront(3);
+        list.PrintList();
+    }
+
+
+
+}
+
+}
